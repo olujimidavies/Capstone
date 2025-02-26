78d08f3734f62505e055534d12d465d8c1372631
Ben Cantrell <ben.cantrell@okstate.edu>
2025-02-25 19:58:47 -0600

    Some files for the DB, but mostly cooked

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import axios from 'axios';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+function Index() {
+  const [users, setUsers] = useState([]);
+
+  // Fetch data from backend API
+  useEffect(() => {
+    axios.get('mysql://root:agcbXbJFrpSeDmIpRYBaEGaqLbKUPCCw@shinkansen.proxy.rlwy.net:17515/railway')  // Replace with your backend URL
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <App users={users} />  {/* Pass users data to App component */}
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Index />);
